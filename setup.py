053dd28a3cb8e24f90a136c7e3dec9b72030ac98
Alon Reznik <alonreznik@gmail.com>
2018-05-10 00:36:28 +0300

    fix setup.py classifies

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
     ],
 )
